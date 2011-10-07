2a3cbc2d921f4a06069262d72f861432dfc0af27
Ryan McDougall <sempuki1@gmail.com>
2011-10-06 23:16:07 -0700

    update state machine

--- exercises/state.cpp
+++ exercises/state.cpp
@@ -4,74 +4,101 @@
 
 using namespace std;
 
-template <typename State>
+template <typename StateType>
 class Machine
 {
     public:
-        typedef stack<Machine<State> *, vector<Machine<State> *> > Stack;
+        typedef StateType State;
+        typedef std::stack<Machine<State> *, std::vector<Machine<State> *>> Stack;
 
-        Machine (Stack &stack) : stack_(stack) {}
+        Machine (Stack &stack) : stack_ (stack) {}
 
-        void start () { stack_.push(this); }
-        void finish () { stack_.pop(); }
+        void start () { stack_.push (this); entered (); }
+        void finish () { stack_.pop (); exited (); }
 
-        virtual void run (State &state) = 0;
+        virtual void entered () const {}
+        virtual void exited () const {}
+
+        virtual void turn (State &state) = 0;
 
     private:
         Stack   &stack_;
 };
 
-struct PrintMachine : Machine<int>
+template <typename Machine>
+class Engine
 {
-    PrintMachine (Stack &stack) : Machine<int> (stack) {}
+    public:
+        typedef typename Machine::State State;
+        typedef typename Machine::Stack Stack;
 
-    void run (int &state)
-    {
-        cout << "print int: " << state << endl; 
+        Engine() : machine_ (stack_) { machine_.start(); }
 
-        ++state;
-        finish();
-    }
+        bool turn (State &state)
+        {
+            stack_.top()->turn (state);
+            return !stack_.empty();
+        }
+
+    private:
+        Stack   stack_;
+        Machine machine_;
 };
 
-struct MyMachine : Machine<int>
+class PrintMachine : public Machine<int>
 {
-    PrintMachine print1, print2, print3;
+    public:
+        PrintMachine (Stack &stack) : Machine<int> (stack) {}
 
-    MyMachine (Stack &stack) : 
-        Machine<int> (stack), 
-        print1 (stack),
-        print2 (stack),
-        print3 (stack)
-    {}
+        void entered () const { cout << "current state: "; }
+        void exited () const { cout << endl; }
+        
+        void turn (int &state) { cout << state; ++state; finish(); }
+};
 
-    void run (int &state)
-    {
-        switch (state)
+class MyMachine : public Machine<int>
+{
+    public:
+        enum { STATE1, STATE2, STATE3, STATE4 };
+
+        MyMachine (Stack &stack) : 
+            Machine<int> (stack), 
+            printer1_ (stack),
+            printer2_ (stack),
+            printer3_ (stack) {}
+
+        void turn (int &globalstate)
         {
-            case 0: print1.start(); break;
-            case 1: print2.start(); break;
-            case 2: print3.start(); break;
-            case 3: finish(); break;
+            switch (globalstate)
+            {
+                case 0: localstate_ = STATE1; break;
+                case 1: localstate_ = STATE2; break;
+                case 2: localstate_ = STATE3; break;
+                case 3: localstate_ = STATE4; break;
+            }
+
+            switch (localstate_)
+            {
+                case STATE1: printer1_.start(); break;
+                case STATE2: printer2_.start(); break;
+                case STATE3: printer3_.start(); break;
+                case STATE4: finish(); break;
+            }
         }
-    }
+
+    private:
+        PrintMachine printer1_;
+        PrintMachine printer2_;
+        PrintMachine printer3_;
+        int localstate_;
 };
 
 int main ()
 {
-    Machine<int>::Stack stack;
-    Machine<int> *current;
     int state = 0;
+    Engine <MyMachine> statemachine;
 
-    MyMachine machine (stack);
-    machine.start();
-
-    while (!stack.empty())
-    {
-        cout << ":" << stack.size() << endl;
-        current = stack.top();
-        current->run (state);
-    }
+    while (statemachine.turn (state));
 
     return 0;
 }
