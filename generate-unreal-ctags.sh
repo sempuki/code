2da6ba56a79d9c422b309871bf49620d9ea97ed0
Ryan McDougall <sempuki1@gmail.com>
2012-03-01 17:00:43 -0800

    update ctags generation

--- generate-unreal-ctags.sh
+++ generate-unreal-ctags.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export GTAGSFORCECPP=1
-find . -name '*.cpp' -or -name '*.h' > cpp-files.txt
 find . -name '*.uc' > uc-files.txt
-gtags -f cpp-files.txt
 ctags -L uc-files.txt -f tags --options=unreal-ctags.config 
