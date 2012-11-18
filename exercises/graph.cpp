5afdc4a8816242b6efcfbb38e9e50aa54e18287c
Ryan McDougall <sempuki1@gmail.com>
2012-11-17 20:33:58 -0800

    update exercises

--- exercises/graph.cpp
+++ exercises/graph.cpp
@@ -10,266 +10,199 @@
 #include <stack>
 #include <queue>
 
-#include <tr1/functional>
-
 using namespace std;
-using namespace std::tr1;
-using namespace std::tr1::placeholders;
 
-// note: without loss of generality, 
-// all vertices are integer named
-// and edges are pairs of vertices
-
-// Adjacency Matrix-based implementation
-class DenseGraph
+namespace graph
 {
-    public:
-        typedef vector <bool> AdjacencyVector;
-        typedef vector <AdjacencyVector> AdjacencyMatrix;
+    typedef int                     Vertex;
+    typedef std::vector<Vertex>     VertexList;
+    typedef std::vector<VertexList> AdjacencyList;
 
-        class EdgeIterator : public std::iterator <std::forward_iterator_tag, int>
-        {
-            public:
-                EdgeIterator (const AdjacencyVector &v, int p = 0) 
-                    : v_ (v), p_ (p) 
-                {
-                    // find first adjacent vertex
-                    for (; (p_ < v_.size()) && !v_[p_]; ++p_); 
-                }
+    typedef VertexList::iterator            VertexIterator;
+    typedef VertexList::const_iterator      VertexConstIterator;
 
-                int &operator* ()
-                { 
-                    return p_; 
-                }
+    typedef AdjacencyList::iterator         EdgeIterator;
+    typedef AdjacencyList::const_iterator   EdgeConstIterator;
 
-                EdgeIterator &operator++ () 
-                { 
-                    for (++p_; (p_ < v_.size()) && !v_[p_]; ++p_); 
-                    return *this; 
-                }
-
-                EdgeIterator operator++ (int) 
-                { 
-                    EdgeIterator t (*this);  
-                    ++ (*this);
-                    return t; 
-                }
-
-                bool operator== (const EdgeIterator &rhs) const 
-                { 
-                    return (v_ == rhs.v_) && (p_ == rhs.p_); 
-                }
-                
-                bool operator!= (const EdgeIterator &rhs) const 
-                { 
-                    return !(*this == rhs); 
-                }
-
-            private:
-                const AdjacencyVector &v_;
-                int p_;
-        };
-
-        DenseGraph (int n) :
-            edges_ (0)
-        {
-            resize_ (n);
-        }
-
-        size_t vertices () const { return adj_.size(); }
-        size_t edges () const { return edges_; }
-
-        EdgeIterator edge_begin (int v) const { return EdgeIterator (adj_[v], 0); }
-        EdgeIterator edge_end (int v) const { return EdgeIterator (adj_[v], adj_[v].size()); }
-
-        void add () 
-        { 
-            resize_ (vertices() + 1);
-        }
-
-        void remove (int v)
-        {
-            for (int i = 0; i < vertices(); ++i)
-                cut (i, v);
-        }
-
-        bool joined (int v, int w) const 
-        { 
-            return (adj_[v][w] || adj_[w][v]);
-        }
-
-        void join (int v, int w)
-        {
-            if (!joined (v, w))
-            {
-                ++ edges_;
-                adj_[v][w] = 
-                    adj_[w][v] = true;
-            }
-        }
-
-        void cut (int v, int w)
-        {
-            if (joined (v, w))
-            {
-                -- edges_;
-                adj_[v][w] = 
-                    adj_[w][v] = false;
-            }
-        }
-
-    private:
-        void resize_ (int n)
-        {
-            adj_.resize (n);
-            for_each (adj_.begin(), adj_.end(), 
-                    bind (&AdjacencyVector::resize, _1, n, false));
-        }
-
-    private:
-        AdjacencyMatrix adj_;
-        size_t          edges_;
-};
-
-// Adjacency List-based implementation
-class SparseGraph
-{
-    public:
-        typedef vector <int> AdjacencyVector;
-        typedef vector <AdjacencyVector> AdjacencyList;
-
-        typedef AdjacencyVector::const_iterator EdgeIterator;
-
-        SparseGraph (int n) :
-            edges_ (0)
-        {
-            resize_ (n);
-        }
-
-        size_t vertices () const { return adj_.size(); }
-        size_t edges () const { return edges_; }
-
-        EdgeIterator edge_begin (int v) const { return adj_[v].begin(); }
-        EdgeIterator edge_end (int v) const { return adj_[v].end(); }
-
-        void add () 
-        { 
-            resize_ (vertices() + 1);
-        }
-
-        void remove (int v)
-        {
-            for (int i = 0; i < vertices(); ++i)
-                cut (i, v);
-        }
-
-        bool joined (int v, int w) const 
-        { 
-            return (find_ (adj_[v], w) || find_ (adj_[w], v));
-        }
-
-        void join (int v, int w)
-        {
-            if (!joined (v, w))
-            {
-                ++ edges_;
-                insert_ (adj_[v], w);
-                insert_ (adj_[w], v);
-            }
-        }
-
-        void cut (int v, int w)
-        {
-            if (joined (v, w))
-            {
-                -- edges_;
-                erase_ (adj_[v], w);
-                erase_ (adj_[w], v);
-            }
-        }
-
-    private:
-        bool find_ (const AdjacencyVector &v, int w) const
-        {
-            return (find (v.begin(), v.end(), w) != v.end());
-        }
-
-        void insert_ (AdjacencyVector &v, int w)
-        {
-            v.push_back (w);
-        }
-
-        void erase_ (AdjacencyVector &v, int w)
-        {
-            v.erase (find (v.begin(), v.end(), w));
-        }
-
-        void resize_ (int n)
-        {
-            adj_.resize (n);
-        }
-
-    private:
-        AdjacencyList   adj_;
-        size_t          edges_;
-};
-
-// note: 
-// * DenseGraph: O(V^2) since each vertex is visited, with a search of all
-// vertices via the EdgeIterator::operator++()
-// * SparseGraph: O(V+E) since each vertex is visited, with a search of all
-// edges via the EdgeIterator::operator++()
-//
-// TODO: separate expanding from contracting of the stack based on whether 
-// what you pop has been visited before, to allow processing on first 
-// visiting the parent, or when all children have been visited. This mirrors 
-// what's possible using a recursive solution (processing before or after the 
-// recursive call).
-
-template <typename Graph>
-struct TraverseDFS
-{
-    const Graph &graph; 
-    TraverseDFS (const Graph &g) : graph (g) {}
-
-    void operator() (function <void(int)> fn, int start)
+    class SparseGraph
     {
-        vector <bool> visited (graph.vertices());
-        stack <int> next;
+        public:
+            SparseGraph (size_t nv = 1) : adjacent_ {nv}, nedges_ {0} {}
 
-        visited [start] = true;
-        next.push (start);
+        public:
+            size_t nedges () const { return nedges_; }
+            size_t nvertices () const { return adjacent_.size(); }
 
-        for (int v; next.size ();)
-        {
-            v = next.top ();
-            next.pop ();
+        public:
+            VertexIterator edge_begin (int v) { return adjacent_[v].begin(); }
+            VertexConstIterator edge_begin (int v) const { return adjacent_[v].begin(); }
 
-            fn (v); // apply operator
+            VertexIterator edge_end (int v) { return adjacent_[v].end(); }
+            VertexConstIterator edge_end (int v) const { return adjacent_[v].end(); }
 
-            typename Graph::EdgeIterator i = graph.edge_begin (v);
-            typename Graph::EdgeIterator e = graph.edge_end (v);
-            for (; i != e; ++i)
-            {
-                if (!visited [*i]) 
+            EdgeIterator vertex_begin () { return adjacent_.begin(); }
+            EdgeConstIterator vertex_begin () const { return adjacent_.begin(); }
+
+            EdgeIterator vertex_end () { return adjacent_.end(); }
+            EdgeConstIterator vertex_end () const { return adjacent_.end(); }
+
+        public:
+            bool has_edge (int v, int w) const 
+            { 
+                bool result = false;
+
+                if (v < nvertices ())
                 {
-                    visited [*i] = true;
-                    next.push (*i);
+                    VertexList const &connected {adjacent_[v]};
+                    VertexConstIterator itr {std::begin (connected)};
+                    VertexConstIterator end {std::end (connected)};
+
+                    result = std::find (itr, end, w) != end;
+                }
+
+                return result;
+            }
+
+            void join (int v, int w)
+            {
+                if (std::max (v, w) >= nvertices ())
+                {
+                    // resize vertices to accommodate all edges
+                    adjacent_.resize (std::max (v, w) + 1);
+                }
+
+                VertexList &connected = adjacent_[v];
+                VertexIterator itr {std::begin (connected)};
+                VertexIterator end {std::end (connected)};
+
+                // try to keep vertices in sorted order
+                for (; itr != end && *itr < w; ++itr);
+
+                connected.insert (itr, w);
+                ++ nedges_;
+            }
+
+            void cut (int v, int w)
+            {
+                if (v < nvertices ())
+                {
+                    VertexList &connected = adjacent_[v];
+                    VertexIterator itr {std::begin (connected)};
+                    VertexIterator end {std::end (connected)};
+
+                    if ((itr = std::find (itr, end, w)) != end)
+                    {
+                        connected.erase (itr);
+                        -- nedges_;
+                    }
+                }
+            }
+
+        private:
+            AdjacencyList   adjacent_;
+            size_t          nedges_;
+    };
+
+
+    // TODO: separate expanding from contracting of the stack based on whether 
+    // what you pop has been visited before, to allow processing on first 
+    // visiting the parent, or when all children have been visited. This mirrors 
+    // what's possible using a recursive solution (processing before or after the 
+    // recursive call).
+
+    template <typename Graph, typename Fringe>
+    struct Traversal
+    {
+        Graph const &graph; 
+        
+        std::vector<int>    order;
+        std::vector<Vertex> parent;
+        std::vector<bool>   visited;
+        std::vector<bool>   processed;
+
+        Traversal (const Graph &g) : 
+            graph (g) 
+        {
+            size_t N = graph.nvertices();
+
+            order.resize (N);
+            parent.resize (N);
+            visited.resize (N);
+            processed.resize (N);
+
+            reset();
+        }
+
+        template <typename Visitor, typename Processor>
+        void operator() (int start, Visitor visit, Processor process)
+        {
+            int vertex, time = 0;
+
+            Fringe fringe;
+            fringe.push (start);
+
+            while (fringe.size() > 0) 
+            {
+                vertex = fringe.next();
+
+                if (!visited [vertex])
+                {
+                    // expand on visit
+                    
+                    visit (vertex);
+                    visited [vertex] = true;
+                    order [vertex] = time ++;
+
+                    auto itr = graph.edge_begin (vertex);
+                    auto end = graph.edge_end (vertex);
+
+                    for (; itr != end; ++itr)
+                    {
+                        parent [*itr] = vertex;
+                        fringe.push (*itr);
+                    }
+                }
+                else
+                {
+                    // collapse on process
+
+                    process (vertex);
+                    processed [vertex] = true;
+
+                    fringe.pop ();
                 }
             }
         }
-    }
-};
 
-void print (int v) { cout << "found vertex: " << v << endl; }
+        void reset ()
+        {
+            std::fill (std::begin(order), std::end(order),          -1);
+            std::fill (std::begin(parent), std::end(parent),        -1);
+            std::fill (std::begin(visited), std::end(visited),      false);
+            std::fill (std::begin(processed), std::end(processed),  false);
+        }
+    };
 
+    class stack : public std::stack<Vertex>
+    {
+        public:
+            Vertex &next () { return std::stack<Vertex>::top(); }
+            Vertex const &next () const { return std::stack<Vertex>::top(); }
+    };
+
+    class queue : public std::queue<Vertex>
+    {
+        public:
+            Vertex &next () { return std::queue<Vertex>::front(); }
+            Vertex const &next () const { return std::queue<Vertex>::front(); }
+    };
+}
 
 //=============================================================================
-// Main entry point
-int
-main (int argc, char** argv)
+
+int main (int argc, char** argv)
 {
-    DenseGraph graph (7);
+    graph::SparseGraph graph;
     graph.join (0, 1);
     graph.join (0, 2);
     graph.join (1, 3);
@@ -277,18 +210,19 @@ main (int argc, char** argv)
     graph.join (2, 5);
     graph.join (2, 6);
 
-    for (int v = 0; v < graph.vertices(); ++v)
+    for (int v = 0; v < graph.nvertices(); ++v)
     {
         cout << "vertex " << v << " is joined with : ";
-        DenseGraph::EdgeIterator i = graph.edge_begin (v);
-        DenseGraph::EdgeIterator e = graph.edge_end (v);
+        auto i = graph.edge_begin (v);
+        auto e = graph.edge_end (v);
         for (; i != e; ++i)
             cout << *i << ", ";
         cout << endl;
     }
 
-    TraverseDFS <DenseGraph> traverse (graph);
-    traverse (print, 0);
+    graph::Traversal <graph::SparseGraph, graph::stack> traverse (graph);
+    traverse (0, [](int v) { cout << "visited: " << v << endl; }, 
+                 [](int v) { cout << "processed: " << v << endl; });
 
     return 0;
 }
