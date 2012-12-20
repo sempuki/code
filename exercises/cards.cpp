040094a9058101be26da47a902a18391471f7a28
Ryan McDougall <sempuki1@gmail.com>
2012-12-19 20:29:43 -0800

    update exercises

--- exercises/cards.cpp
+++ exercises/cards.cpp
@@ -9,6 +9,23 @@
 
 using namespace std;
 
+// To ensure I understood the problem correctly, I translated the wording
+// into code as literally as I was able, which required the use of higher-
+// level primitives to prototype with. I then compared the output of the
+// reference implementation with hand computations on paper, and fixed one
+// or the other as necessary until I was convinced that both my understanding
+// and the reference implementation was correct.
+//
+// I using my intuition compared against the reference results, I mapped out
+// the minimum required data and computation needed to solve the problem as
+// requested. I then implemented an optimized solution, comparing the results
+// it created against the reference implementation to ensure correctness.
+//
+// When the solution was completed after testing, I decided to leave the 
+// reference implementation in place as a form of documentation of the 
+// standard I used for correctness (in case the reference itself was 
+// incorrect).
+
 int string_to_int (const char *str)
 {
     bool leading = true;
@@ -83,32 +100,44 @@ int do_reference_impl (int const N)
     return rounds;
 }
 
+
 int do_optimized_impl (int const N)
 {
-    int A[N], B[N], C[N];
+    int A[N], B[N];
 
     int rounds = 0;
     bool equal = false;
 
-    int *input = A;
-    int *table = B;
-    int *deck = C;
+    int *table = A;
+    int *deck = B;
+
+    // initialize deck into sequential order
 
     for (int n = 0; n < N; ++n)
-        input[n] = deck[n] = n;
+        deck[n] = n;
 
     do 
     {
-        int start = 0;
-        int index = 0;
-        int stride = 1;
-        int next = N-1;
-        bool take = true;
+        // until our current deck is in sequential
+
+        int start = 0;      // first card in the deck
+        int index = 0;      // index into the deck
+        int stride = 1;     // stride to the next card
+        int next = N-1;     // next card on the table
+        bool take = true;   // take the card from deck to table
+
+        // until our table is full
 
         while (next >= 0)
         {
+            // starting from the first available card in the deck
+            // (depending on whether the last card was taken into the table)
+
             index = start;
             start += take * stride;
+
+            // put cards in the table when take == true
+            // skipping all cards already in the table
 
             while (index < N)
             {
@@ -119,6 +148,8 @@ int do_optimized_impl (int const N)
                 take = !take;
             }
 
+            // expand the stride to skip cards in the table
+
             stride <<= 1;
         }
         
@@ -126,9 +157,11 @@ int do_optimized_impl (int const N)
 
         ++rounds;
 
+        // we have returned when the cards are in sequential order again
+
         equal = true;
         for (int n = 0; n < N && equal; ++n)
-            equal = (input[n] == deck[n]);
+            equal = (deck[n] == n);
     } 
     while (!equal);
 
@@ -145,9 +178,8 @@ int main (int argc, char **argv)
 
     int const N = string_to_int (argv[1]);
 
-    //cout << "number of rounds: " << do_reference_impl (N) << endl;
-    //cout << "===========================" << endl;
-
+    //cout << do_reference_impl (N) << endl;
+    //cout << "=====" << endl;
     cout << do_optimized_impl (N) << endl;
 
     return 0;
