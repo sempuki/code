9d46f92fe9c52a203860b14d91a8d1e991d53f63
Ryan McDougall <sempuki1@gmail.com>
2013-02-19 18:48:44 -0800

    update linkedlist

--- exercises/linkedlist.cpp
+++ exercises/linkedlist.cpp
@@ -1,303 +1,141 @@
-/* linkedlist.cpp -- 
- *
- *			Ryan McDougall
- */
-
 #include <iostream>
 #include <iterator>
-#include <algorithm>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-// Linked List Node
-template <typename T>
-struct Node
+namespace list
 {
-    T value;
-    Node <T> *next;
-
-    Node (Node <T> *p = 0) : next (p) {}
-    Node (T v, Node <T> *p = 0) : value (v), next (p) {}
-};
-
-// Linked List Iterator
-template <typename T>
-class ListIterator : public std::iterator <std::forward_iterator_tag, T>
-{
-    public:
-        typedef ListIterator <T> iterator;
-        typedef Node <T> node;
-
-        ListIterator (node *p = 0) : p_ (p) {}
-
-        operator node *() const { return p_; }
-        iterator operator= (node *p) { p_ = p; return *this; }
-
-        T &operator* () const { return p_->value; }
-        T *operator-> () const { return &(p_->value); }
-
-        bool operator== (const iterator &rhs) const { return p_ == rhs.p_; }
-        bool operator!= (const iterator &rhs) const { return p_ != rhs.p_; }
-
-        iterator next () { return p_->next; }
-        void next (iterator n) { p_->next = n; }
-
-        iterator &operator++ () { p_ = next(); return *this; }
-        iterator operator++ (int) { iterator t (*this); p_ = next(); return t; }
-
-    private:
-        node *p_;
-};
-
-// Linked List Container
-template <typename T>
-class List
-{
-    public:
-        typedef ListIterator <T> iterator;
-        typedef Node <T> node;
-
-        List () : sentinel_ (new node) {}
-        ~List () { erase (begin(), end()); }
-
-        iterator begin () { return sentinel_.next(); }
-        iterator end () { return iterator(); }
-
-        T &front () { return *(begin()); }
-
-        // note: O(n)
-        iterator previous (iterator pos)
-        {
-            iterator prev = sentinel_; 
-            iterator i = begin();
-
-            for (; i != pos; ++i)
-                prev = i;
-
-            return prev;
-        }
-
-        // note: O(n)
-        size_t size () const
-        {
-            size_t size;
-            iterator i = begin();
-            iterator e = end();
-            for (; i != e; ++i)
-                ++ size;
-            return size;
-        }
-
-        void swap (List &rhs)
-        {
-            std::swap (sentinel_, rhs.sentinel_);
-        }
-
-        void push_front (const T &v)
-        {
-            insert (begin(), v);
-        }
-
-        void pop_front ()
-        {
-            erase (begin(), begin().next());
-        }
-
-        void insert (iterator pos, const T &v) 
-        {
-            insert_ (previous (pos), v);
-        }
-
-        void insert_after (iterator pos, const T &v) 
-        { 
-            insert_ (pos, v);
-        }
-
-        // note: [begin, end)
-        void insert_after (iterator pos, iterator begin, iterator end)
-        { 
-            for (iterator i (begin); i != end; ++i, ++pos)
-                insert_ (pos, *i);
-        }
-
-        void splice (iterator pos, List &rhs, iterator s)
-        {
-            splice_ (pos, rhs, previous (s), s);
-        }
-
-        // note: [begin, end)
-        void splice (iterator pos, List &rhs, iterator begin, iterator end)
-        {
-            splice_ (pos, rhs, previous (begin), previous (end));
-        }
-
-        void splice_after (iterator pos, List &rhs, iterator prev)
-        {
-            splice_ (pos, rhs, prev, prev.next());
-        }
-
-        // note: (prev, last]
-        void splice_after (iterator pos, List &rhs, iterator prev, iterator last)
-        {
-            splice_ (pos, rhs, prev, last);
-        }
-
-        void erase (iterator pos)
-        {
-            erase_ (previous (pos), pos.next());
-        }
-
-        // note: [begin, end)
-        void erase (iterator begin, iterator end)
-        {
-            erase_ (previous (begin), end);
-        }
-
-        // note: (prev, last]
-        void erase_after (iterator prev, iterator last)
-        {
-            erase_ (prev, last.next());
-        }
-
-        void reverse ()
-        {
-            sentinel_.next (reverse_ (begin()));
-        }
-
-        // note: O(n+m)
-        void merge (List &rhs)
-        {
-            iterator s1 (sentinel_);
-            iterator s2 (rhs.sentinel_);
-            iterator e = end();
-
-            for (;;)
+    template <typename T>
+    class linked
+    {
+        public:
+            struct node
             {
-                if (s1.next() && s2.next())
-                {
-                    if (*s2.next() < *s1.next())
-                        splice_after (s1, rhs, s2);
-                    else
-                        ++ s1;
-                }
-                else
-                {
-                    if (s1.next() == e)
-                        while (s2.next())
-                            splice_after (s1, rhs, s2);
+                T     value;
+                node *next = nullptr;
 
-                    if (s2.next() == e)
-                        break;
-                }
-            }
-        }
+                node (T v, node *n) : 
+                    value {std::move (v)}, next {n} {}
+            };
 
-        // note: O(nlgn)
-        void sort ()
-        {
-        }
-
-        // return node m steps from the end
-        iterator reverse_at (int m)
-        {
-            iterator node;
-            iterator i = begin();
-            iterator e = end();
-
-            for (int n=0; i != e; ++i, ++n)
+            class iterator : public std::iterator <std::forward_iterator_tag, T>
             {
-                if (n == m)
-                    node = begin();
-                else if (n > m)
-                    ++ node;
+                friend class linked;
+
+                public:
+                    iterator () = default;
+                    iterator (iterator const &) = default;
+                    iterator (node *n) : ptr_ {n} {}
+
+                    operator bool () { return ptr_ != nullptr; }
+
+                    bool operator== (iterator const &rhs) { return ptr_ == rhs.ptr_; }
+                    bool operator!= (iterator const &rhs) { return ptr_ != rhs.ptr_; }
+
+                    T &operator* () { return ptr_->value; }
+                    T *operator-> () { return &(ptr_->value); }
+
+                    iterator &operator++ () 
+                    { 
+                        ptr_ = ptr_->next; 
+                        return *this; 
+                    }
+
+                    iterator operator++ (int) 
+                    { 
+                        iterator copy {*this}; 
+                        ptr_ = ptr_->next; 
+                        return copy; 
+                    }
+
+                private:
+                    node  *node_ptr () { return ptr_; }
+                    node **next_ptr () { return &ptr_->next; }
+
+                private:
+                    node *ptr_ = nullptr;
+            };
+
+            // TODO: move constructor/swap 
+
+        public:
+            size_t size () const { return size_; }
+            bool empty () const { return head_ == nullptr; }
+            T &front () { return head_->value; }
+
+        public:
+            iterator begin () { return iterator {head_}; }
+            iterator end () { return iterator {nullptr}; }
+
+        public:
+            iterator previous (iterator position)
+            {
+                iterator prev; 
+
+                for (auto iter = begin(); iter != end(); ++iter)
+                    if (iter != position)
+                        prev = iter;
+                    else break;
+
+                return prev;
             }
 
-            return node;
-        }
-
-    private:
-        // note: insert after
-        void insert_ (iterator pos, const T &v)
-        {
-            if (begin() == end())
-                sentinel_.next (new node (v, 0));
-            else
-                pos.next (new node (v, pos.next()));
-        }
-
-        // note: (prev, end)
-        void erase_ (iterator prev, iterator end)
-        {
-            iterator i = prev.next();
-            while (i != end)
-                delete i++;
-        
-            prev.next (end);
-        }
-
-        // note: (prev, last]
-        void splice_ (iterator pos, List &rhs, iterator prev, iterator last)
-        {
-            iterator succ = pos.next();
-            pos.next (prev.next());
-            prev.next (last.next());
-            last.next (succ);
-        }
-        
-        // note: returns reversed head
-        iterator reverse_ (iterator head)
-        { 
-            iterator i = head;
-            iterator curr, res;
-
-            for (;;)
+        public:
+            iterator push_front (T const &value)
             {
-                if (i.next())
-                {
-                    curr = i++;
-                    curr.next (res);
-                    res = curr;
-                }
-                else
-                {
-                    head = i;
-                    head.next (res);
-                    break;
-                }
+                node **head = &head_;
+                return iterator {insert_ (head, value)};
             }
 
-            return head;
-        }
+            iterator insert_after (iterator position, T const &value)
+            {
+                node **head = position.next_ptr ();
+                return iterator {insert_ (head, value)};
+            }
 
-    private:
-        iterator    sentinel_;
-};
+            iterator insert_before (iterator position, T const &value)
+            {
+                return insert_after (previous (position), value);
+            }
 
-//=============================================================================
-// Main entry point
-int
-main (int argc, char** argv)
+        public:
+            void erase (iterator begin, iterator end)
+            {
+                iterator prev = previous (begin);
+
+            }
+
+        private:
+            node *insert_ (node **head, T const &value)
+            {
+                ++size_;
+                *head = new node {value, *head};
+                return *head;
+            }
+
+            void erase_ ()
+            {
+            }
+
+        private:
+            node  *head_ = nullptr;
+            size_t size_ = 0;
+    };
+}
+
+int main (int argc, char **argv)
 {
-    List <int> a, b;
+    list::linked<int> list;
 
-    a.push_front (5);
-    a.push_front (4);
-    a.push_front (2);
-    a.push_front (1);
+    list.push_front (1);
+    list.push_front (2);
+    list.push_front (3);
 
-    List<int>::iterator i = find (a.begin(), a.end(), 2);
-    a.insert_after (i, 3);
-    a.erase (i);
+    list.insert_before (std::end (list), 4);
 
-    b.push_front (0);
-    b.insert_after (b.begin(), a.begin(), a.end());
-    a.reverse ();
-    b.splice_after (b.begin(), a, a.previous (a.begin()), a.begin().next());
-    a.swap (b);
-    b.pop_front ();
+    for (auto v : list)
+        cout << v << endl;
 
-    copy (a.begin(), a.end(), ostream_iterator <int> (cout, " ")); cout << endl;
-    copy (b.begin(), b.end(), ostream_iterator <int> (cout, " ")); cout << endl;
-    
     return 0;
 }
