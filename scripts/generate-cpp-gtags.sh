6db74f2b1949c8ada78ca43a382f0e5db71ed330
Ryan McDougall <sempuki1@gmail.com>
2013-04-24 20:42:29 -0700

    update tags generation script

--- scripts/generate-cpp-gtags.sh
+++ scripts/generate-cpp-gtags.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 export GTAGSFORCECPP=1
-find . -name '*.cpp' -or -name '*.h' > cpp-files.txt
+find . \
+    -name '*.cpp' -or \
+    -name '*.cxx' -or \
+    -name '*.cc' -or \
+    -name '*.c' -or \
+    -name '*.hpp' -or \
+    -name '*.hxx' -or \
+    -name '*.hh' -or \
+    -name '*.h' > cpp-files.txt
 gtags -f cpp-files.txt
