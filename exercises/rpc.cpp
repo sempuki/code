df5cb956f75587530d5a3bd89bb02b9959c50782
Ryan McDougall <sempuki1@gmail.com>
2013-05-20 22:40:08 -0700

    update exercises

--- exercises/rpc.cpp
+++ exercises/rpc.cpp
@@ -21,7 +21,8 @@ namespace memory
         union 
         {
             Type *items = nullptr;
-            void const *address;
+            void const *pointer;
+            uintptr_t   address;
         };
 
         size_t bytes = 0;
@@ -31,14 +32,20 @@ namespace memory
         buffer (Type *data, size_t num_items) : 
             items {data}, bytes {num_items * sizeof (Type)} {}
 
+        buffer (Type *begin, Type *end) : 
+            items {begin}, bytes {(end - begin) * sizeof (Type)} {}
+
         buffer (void const *data, size_t num_bytes) :
-            address {data}, bytes {num_bytes} {}
+            pointer {data}, bytes {num_bytes} {}
+
+        buffer (void const *begin, void const *end) :
+            pointer {begin}, bytes {(uintptr_t) end - (uintptr_t) begin} {}
 
         template <typename U> 
         buffer (buffer<U> const &copy) :
-            buffer {copy.address, copy.bytes} {}
+            buffer {copy.pointer, copy.bytes} {}
 
-        operator bool () const { return address != nullptr && bytes != 0; }
+        operator bool () const { return pointer != nullptr; }
         size_t size () const { return bytes / sizeof (Type); }
     };
 
@@ -61,25 +68,25 @@ namespace memory
     Type const *end (buffer<Type> const &buf) { return begin (buf) + size (buf); }
 
     template <typename Type>
-    buffer<Type> shrink (buffer<Type> &buf, int amount)
-    {
-        return {begin (buf), size (buf) - amount};
-    }
-
-    template <typename Type>
-    buffer<Type> grow (buffer<Type> &buf, int amount)
+    buffer<Type> grow (buffer<Type> &buf, size_t amount)
     {
         return {begin (buf), size (buf) + amount};
     }
 
     template <typename Type>
-    buffer<Type> advance (buffer<Type> &buf, int amount)
+    buffer<Type> shrink (buffer<Type> &buf, size_t amount)
+    {
+        return {begin (buf), size (buf) - amount};
+    }
+
+    template <typename Type>
+    buffer<Type> advance (buffer<Type> &buf, size_t amount)
     {
         return {begin (buf) + amount, size (buf)};
     }
 
     template <typename Type>
-    buffer<Type> retreat (buffer<Type> &buf, int amount)
+    buffer<Type> retreat (buffer<Type> &buf, size_t amount)
     {
         return {begin (buf) - amount, size (buf)};
     }
@@ -91,92 +98,91 @@ namespace memory
     }
 
     template <typename T, typename U>
-    bool is_before (buffer<T> const &a, buffer<U> const &b)
+    bool preceeds (buffer<T> const &a, buffer<U> const &b)
     {
         return a.address < b.address;
     }
 
     template <typename T, typename U>
-    bool is_smaller (buffer<T> const &a, buffer<U> const &b)
+    bool smaller (buffer<T> const &a, buffer<U> const &b)
     {
         return a.bytes < b.bytes;
-    }
-
-    template <typename T, typename U>
-    ptrdiff_t distance (buffer<T> const &a, buffer<U> const &b)
-    {
-        return reinterpret_cast<intptr_t> (b.address) - 
-            reinterpret_cast<intptr_t> (a.address);
-    }
-    
-    template <typename T, typename U>
-    ssize_t difference (buffer<T> const &a, buffer<U> const &b)
-    {
-        return b.bytes - a.bytes;
     }
 }
 
 namespace data { namespace map {
 
-    namespace basic
+    namespace native
     {
+        // Buffer .............................................................
+
+        template <typename T>
+        size_t commit_size (memory::buffer<T> const &buf, T value)
+        {
+            return sizeof (value);
+        }
+
+        template <typename T>
+        bool can_insert (memory::buffer<T> const &buf, T value)
+        {
+            return buf.bytes >= commit_size (buf, value);
+        }
+
         template <typename T>
         memory::buffer<T> &operator<< (memory::buffer<T> &buf, T value)
         {
-            if (buf.bytes >= sizeof (T))
-            {
-                *begin (buf) = value; 
-                buf = offset (buf, 1);
-            }
-            else
-                buf = {};
+            *begin (buf) = value; 
+            return buf = offset (buf, 1);
+        }
 
-            return buf;
+        template <typename T>
+        bool can_extract (memory::buffer<T> const &buf, T value)
+        {
+            return buf.bytes >= commit_size (buf, value);
         }
 
         template <typename T>
         memory::buffer<T> &operator>> (memory::buffer<T> &buf, T &value)
         {
-            if (buf.bytes >= sizeof (T))
-            {
-                value = *begin (buf);
-                buf = offset (buf, 1);
-            }
-            else
-                buf = {};
-
-            return buf;
+            value = *begin (buf); 
+            return buf = offset (buf, 1);
         }
     }
 
     namespace network
     {
+        // Buffer .............................................................
+
+        template <typename T>
+        size_t commit_size (memory::buffer<T> &buf, T value)
+        {
+            return sizeof (value);
+        }
+
+        template <typename T>
+        bool can_insert (memory::buffer<T> &buf, T value)
+        {
+            return buf.bytes >= commit_size (buf, value);
+        }
+
         template <typename T>
         memory::buffer<T> &operator<< (memory::buffer<T> &buf, T value)
         {
-            if (buf.bytes >= sizeof (T))
-            {
-                *begin (buf) = make_network_byte_order (value); 
-                buf = offset (buf, 1);
-            }
-            else
-                buf = {};
+            *begin (buf) = make_network_byte_order (value); 
+            return buf = offset (buf, 1);
+        }
 
-            return buf;
+        template <typename T>
+        bool can_extract (memory::buffer<T> &buf, T value)
+        {
+            return buf.bytes >= commit_size (buf, value);
         }
 
         template <typename T>
         memory::buffer<T> &operator>> (memory::buffer<T> &buf, T &value)
         {
-            if (buf.bytes >= sizeof (T))
-            {
-                value = make_host_byte_order (*begin (buf));
-                buf = offset (buf, 1);
-            }
-            else
-                buf = {};
-
-            return buf;
+            value = make_host_byte_order (*begin (buf)); 
+            return buf = offset (buf, 1);
         }
     }
 
@@ -184,36 +190,78 @@ namespace data { namespace map {
 
 namespace core {
 
-    namespace policy { namespace data { namespace dispatch {
+    namespace policy { namespace data { namespace mapper {
 
-        struct basic
+        struct native
         {
-            template <typename Destination, typename Type>
-            Destination &on_insert (Destination &destination, Type input)
+            template <typename Container, typename Type>
+            size_t commit_size (Container const &container, Type const &type)
             {
-                using ::data::map::basic::operator<<;
+                using ::data::map::native::commit_size;
+                return commit_size (container, type);
+            }
+
+            template <typename Destination, typename Type>
+            bool can_insert (Destination const &destination, Type const &input)
+            {
+                using ::data::map::native::can_insert;
+                return can_insert (destination, input);
+            }
+
+            template <typename Destination, typename Type>
+            Destination &insert (Destination &destination, Type input)
+            {
+                using ::data::map::native::operator<<;
                 return destination << input;
             }
             
             template <typename Source, typename Type>
-            Source &on_extract (Source &source, Type &output)
+            bool can_extract (Source const &source, Type const &output)
             {
-                using ::data::map::basic::operator>>;
+                using ::data::map::native::can_extract;
+                return can_extract (source, output);
+            }
+
+            template <typename Source, typename Type>
+            Source &extract (Source &source, Type &output)
+            {
+                using ::data::map::native::operator>>;
                 return source >> output;
             }
         };
 
         struct network
         {
+            template <typename Container, typename Type>
+            size_t commit_size (Container const &container, Type const &type)
+            {
+                using ::data::map::native::commit_size;
+                return commit_size (container, type);
+            }
+
             template <typename Destination, typename Type>
-            Destination &on_insert (Destination &destination, Type input)
+            bool can_insert (Destination const &destination, Type const &input)
+            {
+                using ::data::map::network::can_insert;
+                return can_insert (destination, input);
+            }
+
+            template <typename Destination, typename Type>
+            Destination &insert (Destination &destination, Type input)
             {
                 using ::data::map::network::operator<<;
                 return destination << input;
             }
             
             template <typename Source, typename Type>
-            Source &on_extract (Source &source, Type &output)
+            bool can_extract (Source const &source, Type const &output)
+            {
+                using ::data::map::network::can_extract;
+                return can_extract (source, output);
+            }
+
+            template <typename Source, typename Type>
+            Source &extract (Source &source, Type &output)
             {
                 using ::data::map::network::operator>>;
                 return source >> output;
@@ -222,20 +270,20 @@ namespace core {
 
     } } }
 
-    template <typename E, typename Dispatcher>
-    class stream : private Dispatcher
+    template <typename E, typename IO>
+    class stream : private IO
     {
         public:
             stream (memory::buffer<E> const &buf)
-                : buf_ {buf}, size_ {0}, read_ {0}, write_ {0} 
+                : buf_ {buf}, size_ {0}, rdpos_ {0}, wrpos_ {0} 
             {}
 
             template <typename T>
             stream &operator<< (T const &item)
             {
-                Dispatcher::on_insert (buf_.items[write_], item);
+                IO::insert (buf_.items[wrpos_], item);
 
-                ++write_ %= size (buf_);
+                ++wrpos_ %= size (buf_);
                 ++size_;
 
                 return *this;
@@ -244,9 +292,9 @@ namespace core {
             template <typename T>
             stream &operator>> (T &item)
             {
-                Dispatcher::on_extract (buf_.items[read_], item);
+                IO::extract (buf_.items[rdpos_], item);
 
-                ++read_ %= size (buf_);
+                ++rdpos_ %= size (buf_);
                 --size_;
 
                 return *this;
@@ -254,40 +302,42 @@ namespace core {
 
             bool full () const { return size_ == size (buf_); }
             bool empty () const { return size_ == 0; }
-            int size () const { return size_; }
+
+            size_t in_capacity () const { return size_; }
+            size_t out_capacity () const { return size (buf_) - size_; } 
 
         private:
             memory::buffer<E> buf_;
-            int size_, read_, write_;
+            int size_, rdpos_, wrpos_;
     };
 
-    template <typename Dispatcher>
-    class stream <uint8_t, Dispatcher> : private Dispatcher
+    template <typename IO>
+    class stream <uint8_t, IO> : private IO
     {
         public:
             stream (memory::buffer<uint8_t> const &buf)
-                : buf_ {buf}, read_ {begin (buf), 0}, write_ {buf}
+                : buf_ {buf}, rdpos_ {0}, wrpos_ {0}, size_ {0}
             {}
 
             template <typename T>
             stream &operator<< (T const &item)
             {
-                memory::buffer<T> basebuf, writebuf;
+                auto forward = rdpos_ <= wrpos_;
+                auto rdbegin = begin (buf_) + rdpos_;
+                auto wrbegin = begin (buf_) + wrpos_;
+                auto wrend = forward? end (buf_) : rdbegin;
+                auto wrbuf = memory::buffer<T> {wrbegin, wrend};
 
-                basebuf = writebuf = write_;
-                Dispatcher::on_insert (writebuf, item);
+                if (!IO::can_insert (wrbuf, item) && forward) // try wrap-around
+                    wrbuf = {begin (buf_), rdbegin};
 
-                if (!writebuf && is_before (read_, write_))
+                if (IO::can_insert (wrbuf, item))
                 {
-                    basebuf = writebuf = {buf_.address, (size_t) distance (buf_, read_)};
-                    Dispatcher::on_insert (writebuf, item);
+                    IO::insert (wrbuf, item);
+                    wrpos_ = wrbuf.address - buf_.address;
+                    size_ += IO::commit_size (wrbuf, item);
                 }
-
-                if (writebuf) 
-                {
-                    size_ += distance (basebuf, writebuf);
-                    write_ = writebuf;
-                }
+                // TODO: else set error flags
 
                 return *this;
             }
@@ -295,45 +345,48 @@ namespace core {
             template <typename T>
             stream &operator>> (T &item)
             {
-                memory::buffer<T> basebuf, readbuf;
-                    
-                basebuf = readbuf = read_;
-                Dispatcher::on_extract (basebuf, item);
+                auto forward = rdpos_ <= wrpos_;
+                auto wrbegin = begin (buf_) + wrpos_;
+                auto rdbegin = begin (buf_) + rdpos_;
+                auto rdend = forward? wrbegin : end (buf_);
 
-                if (!readbuf && is_before (write_, read_))
-                {
-                    basebuf = readbuf = {buf_.address, (size_t) distance (buf_, write_)};
-                    Dispatcher::on_extract (readbuf, item);
-                }
+                auto rdbuf = memory::buffer<T> {rdbegin, rdend};
 
-                if (readbuf) 
+                if (!IO::can_extract (rdbuf, item) && !forward) // try wrap-around
+                    rdbuf = {begin (buf_), wrbegin};
+
+                if (IO::can_extract (rdbuf, item))
                 {
-                    size_ -= distance (basebuf, readbuf);
-                    read_ = readbuf;
+                    IO::extract (rdbuf, item);
+                    rdpos_ = rdbuf.address - buf_.address;
+                    size_ -= IO::commit_size (rdbuf, item);
                 }
+                // TODO: else set error flags
 
                 return *this;
             }
 
-            bool full () const { return size_ == size (buf_); }
+            bool full () const { return size_ == 0; }
             bool empty () const { return size_ == 0; }
-            int size () const { return size_; }
+
+            size_t in_capacity () const { return size_; }
+            size_t out_capacity () const { return size (buf_) - size_; } 
 
         private:
-            memory::buffer<uint8_t> buf_, read_, write_;
-            int size_;
+            memory::buffer<uint8_t> buf_;//, rdbuf_, wrbuf_;
+            int rdpos_, wrpos_, size_;
     };
 
-    template <typename Dispatcher>
-    class stream <bool, Dispatcher> : private Dispatcher
+    template <typename IO>
+    class stream <bool, IO> : private IO
     {
         // TODO
     };
 
     template <typename E>
-    using basicstream = stream <E, policy::data::dispatch::basic>;
-    using bytestream = stream <uint8_t, policy::data::dispatch::basic>;
-    using bitstream = stream <bool, policy::data::dispatch::basic>;
+    using basicstream = stream <E, policy::data::mapper::native>;
+    using bytestream = stream <uint8_t, policy::data::mapper::native>;
+    using bitstream = stream <bool, policy::data::mapper::native>;
 }
 
 int main (int argc, char **argv)
@@ -348,6 +401,13 @@ int main (int argc, char **argv)
     stream << (int32_t) 0xFF00FF00;
     stream << (int32_t) 0x00FF00FF;
     stream << (int32_t) 0xAABBAABB;
+
+    //for (uint8_t i = 0; i < size; ++i)
+    //    stream << i;
+
+    uint8_t i = 0;
+    while (!stream.empty ())
+        stream >> i, cout << std::hex << (int) i << endl;
 
     cout << std::hex;
     for (int i : memory)
