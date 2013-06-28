f16a91e89e489371faf68635d59223962b5590ec
Ryan McDougall <rmcdougall@igware.com>
2013-06-28 10:50:01 -0700

    update exercises

--- exercises/bitbuf.cpp
+++ exercises/bitbuf.cpp
@@ -6,6 +6,81 @@
 
 using namespace std;
 
+template <typename T>
+struct bit_traits;
+
+template <>
+struct bit_traits<int8_t>
+{
+    constexpr static uint8_t zeros = 0;
+    constexpr static uint8_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint8_t) * 8;
+    constexpr static size_t size_log2 = 3;
+};
+
+template <>
+struct bit_traits<uint8_t>
+{
+    constexpr static uint8_t zeros = 0;
+    constexpr static uint8_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint8_t) * 8;
+    constexpr static size_t size_log2 = 3;
+};
+
+template <>
+struct bit_traits<int16_t>
+{
+    constexpr static uint16_t zeros = 0;
+    constexpr static uint16_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint16_t) * 8;
+    constexpr static size_t size_log2 = 4;
+};
+
+template <>
+struct bit_traits<uint16_t>
+{
+    constexpr static uint16_t zeros = 0;
+    constexpr static uint16_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint16_t) * 8;
+    constexpr static size_t size_log2 = 4;
+};
+
+template <>
+struct bit_traits<int32_t>
+{
+    constexpr static uint32_t zeros = 0;
+    constexpr static uint32_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint32_t) * 8;
+    constexpr static size_t size_log2 = 5;
+};
+
+template <>
+struct bit_traits<uint32_t>
+{
+    constexpr static uint32_t zeros = 0;
+    constexpr static uint32_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint32_t) * 8;
+    constexpr static size_t size_log2 = 5;
+};
+
+template <>
+struct bit_traits<int64_t>
+{
+    constexpr static uint64_t zeros = 0;
+    constexpr static uint64_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint64_t) * 8;
+    constexpr static size_t size_log2 = 6;
+};
+
+template <>
+struct bit_traits<uint64_t>
+{
+    constexpr static uint64_t zeros = 0;
+    constexpr static uint64_t ones = ~zeros;
+    constexpr static size_t size = sizeof (uint64_t) * 8;
+    constexpr static size_t size_log2 = 6;
+};
+
 struct bitbuffer
 {
     uint8_t *base = nullptr;
@@ -26,6 +101,30 @@ size_t size (bitbuffer const &buf)
     return buf.limit - buf.offset;
 }
 
+template <typename Type>
+void store (bitbuffer &buf, Type value)
+{
+    // assert (size (buf) >= bit_traits<Type>::size)
+    Type *ptr = (Type *) buf.base + (buf.offset >> bit_traits<Type>::size_log2);
+    Type off = buf.offset & bit_traits<Type>::size-1;
+
+    if (off)
+    {
+        Type dstmask = bit_traits<Type>::ones << off;
+        Type srcmask = bit_traits<Type>::ones >> off;
+        Type frag1 = ((value & srcmask) << off) | (*ptr & ~dstmask);
+        Type frag2 = ((value & ~srcmask) >> off) | (*ptr & dstmask);
+
+        *ptr++ = frag1;
+        *ptr = frag2;
+    }
+    else
+        *ptr = value;
+
+    buf.offset += bit_traits<Type>::size;
+}
+
+template <>
 void store (bitbuffer &buf, bool value)
 {
     uint8_t *ptr = buf.base + (buf.offset >> 3);
@@ -35,6 +134,29 @@ void store (bitbuffer &buf, bool value)
     buf.offset += 1;
 }
 
+
+template <typename Type>
+void load (bitbuffer &buf, Type &value)
+{
+    // assert (size (buf) >= bit_traits<Type>::size)
+    Type *ptr = (Type *) buf.base + (buf.offset >> bit_traits<Type>::size_log2);
+    Type off = buf.offset & bit_traits<Type>::size-1;
+
+    if (off)
+    {
+        Type mask = bit_traits<Type>::ones << off;
+        Type frag1 = *(ptr+0);
+        Type frag2 = *(ptr+1);
+
+        value = ((frag1 & mask) >> off) | ((frag2 & ~mask) << (bit_traits<Type>::size - off));
+    }
+    else
+        value = *ptr;
+
+    buf.offset += bit_traits<Type>::size;
+}
+
+template <>
 void load (bitbuffer &buf, bool &value)
 {
     uint8_t *ptr = buf.base + (buf.offset >> 3);
@@ -44,93 +166,6 @@ void load (bitbuffer &buf, bool &value)
     buf.offset += 1;
 }
 
-void store (bitbuffer &buf, uint8_t value)
-{
-    // assert (size (buf) >= 8)
-    uint8_t ones = ~0;
-    uint8_t *ptr = buf.base + (buf.offset >> 3);
-    uint8_t off = buf.offset & 7;
-
-    if (off)
-    {
-        uint8_t dstmask = ones << off;
-        uint8_t srcmask = ones >> off;
-        uint8_t frag1 = ((value & srcmask) << off) | (*ptr & ~dstmask);
-        uint8_t frag2 = ((value & ~srcmask) >> off) | (*ptr & dstmask);
-
-        *ptr++ = frag1;
-        *ptr = frag2;
-    }
-    else
-        *ptr = value;
-
-    buf.offset += 8;
-}
-
-void load (bitbuffer &buf, uint8_t &value)
-{
-    // assert (size (buf) >= 8)
-    uint8_t ones = ~0;
-    uint8_t *ptr = buf.base + (buf.offset >> 3);
-    uint8_t off = buf.offset & 7;
-
-    if (off)
-    {
-        uint8_t mask = ones << off;
-        uint8_t frag1 = *(ptr+0);
-        uint8_t frag2 = *(ptr+1);
-
-        value = ((frag1 & mask) >> off) | ((frag2 & ~mask) << (8 - off));
-    }
-    else
-        value = *ptr;
-
-    buf.offset += 8;
-}
-
-void store (bitbuffer &buf, uint32_t value)
-{
-    // assert (size (buf) >= 32)
-    uint32_t ones = ~0;
-    uint32_t *ptr = (uint32_t *) buf.base + (buf.offset >> 5);
-    uint32_t off = buf.offset & 31;
-
-    if (off)
-    {
-        uint32_t dstmask = ones << off;
-        uint32_t srcmask = ones >> off;
-        uint32_t frag1 = ((value & srcmask) << off) | (*ptr & ~dstmask);
-        uint32_t frag2 = ((value & ~srcmask) >> off) | (*ptr & dstmask);
-
-        *ptr++ = frag1;
-        *ptr = frag2;
-    }
-    else
-        *ptr = value;
-
-    buf.offset += 32;
-}
-
-void load (bitbuffer &buf, uint32_t &value)
-{
-    // assert (size (buf) >= 32)
-    uint32_t ones = ~0;
-    uint32_t *ptr = (uint32_t *) buf.base + (buf.offset >> 5);
-    uint32_t off = buf.offset & 31;
-
-    if (off)
-    {
-        uint32_t mask = ones << off;
-        uint32_t frag1 = *(ptr+0);
-        uint32_t frag2 = *(ptr+1);
-
-        value = ((frag1 & mask) >> off) | ((frag2 & ~mask) << (32 - off));
-    }
-    else
-        value = *ptr;
-
-    buf.offset += 32;
-}
 
 int main (int argc, char **argv)
 {
@@ -147,7 +182,7 @@ int main (int argc, char **argv)
 
     cout << std::hex;
     cout << "---------------" << endl;
-
+    
     buf.offset = 0;
     bool value1 = false;
     uint8_t value8 = 0;
