05575b7ed0b2d147c269186e21bd2a742e9cde73
Ryan McDougall <sempuki1@gmail.com>
2013-07-19 19:10:15 -0700

    add config make file

--- config/Templates/main_.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class MainClass
-{
-    public static void Main()
-    {
-	Console.WriteLine("Hello World!");
-    }
-}
--- config/Templates/main_.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-# file.py -- purpose
-#
-#               Ryan McDougall
-
-import sys
