111872bc21bc77921b32d8a978a7d6318998d7d2
Ryan McDougall <sempuki1@gmail.com>
2014-01-08 17:40:19 -0800

    update exercises

--- exercises/concurrent.cpp
+++ exercises/concurrent.cpp
@@ -39,7 +39,11 @@ class bounded_queue
 
     public:
         bounded_queue () :
+#ifdef TEST
             head_ {0}, tail_ {0}, size_ {0} {}
+#else
+            head_ {0}, tail_ {0}, head_ticket_ {0}, tail_ticket_ {0} {}
+#endif
 
     public:
         void push (value_type const &value)
@@ -53,11 +57,18 @@ class bounded_queue
 
             available_data.notify_one();
 #else
-            auto head = head_++;
-            while (size_ == N || (head - tail_) >= N);
-            assert (size_ >= 0 && size_ <= N);
-            buffer_[head % N] = value;
-            size_++;
+            auto ticket = head_ticket_.fetch_add (1);
+            auto head = head_.load(); 
+            auto tail = tail_.load();
+
+            while ((head - tail) >= N || ticket != head)
+                head = head_.load(), tail = tail_.load();
+
+            std::atomic_thread_fence (std::memory_order_acquire);
+
+            assert (size() >= 0 && size() <= N);
+            buffer_[ticket % N] = value;
+            head_++;
 #endif
         }
 
@@ -82,11 +93,18 @@ class bounded_queue
 
             available_room.notify_one();
 #else
-            auto tail = tail_++;
-            while (size_ == 0 || (head_ - tail) <= 0);
-            assert (size_ >= 0 && size_ <= N);
+            auto ticket = tail_ticket_.fetch_add (1);
+            auto head = head_.load(); 
+            auto tail = tail_.load();
+
+            while ((head - tail) <= 0 || ticket != tail)
+                head = head_.load(), tail = tail_.load();
+
+            std::atomic_thread_fence (std::memory_order_acquire);
+
+            assert (size() >= 0 && size() <= N);
             value = buffer_[tail % N];
-            size_--;
+            tail_++;
 #endif
         }
 
@@ -101,9 +119,9 @@ class bounded_queue
         }
 
     public:
-        size_type size () const { return size_; }
-        size_type full () const { return size_ == N; }
-        size_type empty () const { return size_ == 0; }
+        size_type size () const { return head_ - tail_; }
+        size_type full () const { return size() == N; }
+        size_type empty () const { return size() == 0; }
 
     private:
 #ifdef TEST
@@ -114,9 +132,11 @@ class bounded_queue
         std::condition_variable available_data;
         std::condition_variable available_room;
 #else
-        std::atomic<int64_t> tail_;
         std::atomic<int64_t> head_;
-        std::atomic<int32_t> size_;
+        std::atomic<int64_t> tail_;
+
+        std::atomic<int64_t> head_ticket_;
+        std::atomic<int64_t> tail_ticket_;
 #endif
 
     private:
@@ -127,42 +147,81 @@ class bounded_queue
 int main (int argc, char **argv)
 {
     bounded_queue<int, 10> queue;
+    std::atomic<int> product {0};
+    std::atomic<int> consumed {0};
+    size_t const production = 100000000;
 
-    std::atomic<bool> start {false};
+    //std::vector<std::atomic<bool>> found (production);
+    //for (auto &v : found) v = false;
 
-    size_t const consumption = 10000000;
-    size_t const production = 10000000;
-
-    std::thread producer {[&]
+    std::thread producer1 {[&]
         {
-            start = true;
             std::cout << "starting producer" << std::endl;
 
-            for (int i=0; i < production; ++i)
-                queue.push (i);
+            int value = 0;
+            while ((value = product++) < production)
+                queue.push (value);
 
             std::cout << "finished producer" << std::endl;
         }};
 
-    std::thread consumer {[&]
+    std::thread producer2 {[&]
+        {
+            std::cout << "starting producer" << std::endl;
+
+            int value = 0;
+            while ((value = product++) < production)
+                queue.push (value);
+
+            std::cout << "finished producer" << std::endl;
+        }};
+
+    std::thread producer3 {[&]
+        {
+            std::cout << "starting producer" << std::endl;
+
+            int value = 0;
+            while ((value = product++) < production)
+                queue.push (value);
+
+            std::cout << "finished producer" << std::endl;
+        }};
+
+    std::thread consumer1 {[&]
         {
             int result = 0;
-            while (start == false);
-            std::cout << "starting consumer" << std::endl;
-
-            for (int i=0; i < consumption; ++i)
+            while (consumed++ < production)
             {
                 queue.pop (result);
-                if (result != i) 
-                    std::cout << result << " / " << i << std::endl;
+                //found[result] = true;
             }
 
-            std::cout << std::endl;
+            std::cout << result << std::endl;
             std::cout << "finished consumer" << std::endl;
         }};
 
-    producer.join();
-    consumer.join();
+    std::thread consumer2 {[&]
+        {
+            int result = 0;
+            while (consumed++ < production)
+            {
+                queue.pop (result);
+                //found[result] = true;
+            }
+
+            std::cout << result << std::endl;
+            std::cout << "finished consumer" << std::endl;
+        }};
+
+    producer1.join();
+    producer2.join();
+    producer3.join();
+    consumer1.join();
+    consumer2.join();
+
+    //int i = 0;
+    //for (auto &v : found)
+    //    std::cout << i++ << " = " << v << std::endl;
 
     return 0;
 }
