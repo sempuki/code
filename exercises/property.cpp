b3b4e48b5094b3461e24428aa4f7d9042f6f60a1
Ryan McDougall <sempuki1@gmail.com>
2014-06-08 14:02:12 -0700

    update exercise

--- exercises/property.cpp
+++ exercises/property.cpp
@@ -4,7 +4,7 @@
 using std::cout;
 using std::endl;
 
-using nullfn = void (void);
+using nullfn_t = void (void);
 
 template <typename Type, typename Result, typename ...Arguments>
 struct property_method_traits {};
@@ -17,90 +17,88 @@ struct property_method_traits <Type, Result (Arguments...)>
 };
 
 template <typename Type, typename AccessType, typename MutateType>
-class basic_property
+class property_base
 {
   public:
-    using access_method = typename property_method_traits<Type, AccessType>::method_type;
-    using access_result = typename property_method_traits<Type, AccessType>::result_type;
+    using property_type = Type;
 
-    using mutate_method = typename property_method_traits<Type, MutateType>::method_type;
-    using mutate_result = typename property_method_traits<Type, MutateType>::result_type;
+    using access_method_type = typename property_method_traits<Type,AccessType>::method_type;
+    using access_result_type = typename property_method_traits<Type,AccessType>::result_type;
+    using mutate_method_type = typename property_method_traits<Type,MutateType>::method_type;
+    using mutate_result_type = typename property_method_traits<Type,MutateType>::result_type;
 
   public:
     template <typename Accessor, typename Mutator>
-    property (Type init, Accessor accessor, Mutator mutator) :
+    property_base (Type init, Accessor accessor, Mutator mutator) :
       property_ {std::forward<Type> (init)},
       accessor_ {std::forward<Accessor> (accessor)},
       mutator_ {std::forward<Mutator> (mutator)} {}
 
-  public:
-    operator access_result ()
-    {
-      return accessor_ (property_);
-    }
-
-    mutate_result operator= (Type &&value)
-    {
-      return mutator_ (property_, std::forward<Type>(value));
-    }
-
-  private:
-    Type property_;
-
-  private:
-    access_method accessor_;
-    mutate_method mutator_;
+  protected:
+    property_type property_;
+    access_method_type accessor_;
+    mutate_method_type mutator_;
 };
 
-template <typename Type, typename AccessType, typename MutateType>
-class property
+template <typename Type, typename AccessType = nullfn_t, typename MutateType = nullfn_t>
+class simple_property : 
+  public property_base<Type, AccessType, MutateType>
 {
-  public:
-    using access_method = typename property_method_traits<Type, AccessType>::method_type;
-    using access_result = typename property_method_traits<Type, AccessType>::result_type;
+  private:
+    using base_type = property_base<Type,AccessType,MutateType>;
+    using property_type = typename base_type::property_type;
+    using access_result_type = typename base_type::access_result_type;
+    using mutate_result_type = typename base_type::mutate_result_type;
 
-    using mutate_method = typename property_method_traits<Type, MutateType>::method_type;
-    using mutate_result = typename property_method_traits<Type, MutateType>::result_type;
+  public:
+    using base_type::base_type;
+
+    operator access_result_type ()
+    {
+      return base_type::accessor_ (base_type::property_);
+    }
+
+    mutate_result_type operator= (property_type &&value)
+    {
+      return base_type::mutator_ (base_type::property_, std::forward<property_type>(value));
+    }
+};
+
+template <typename Type, typename AccessType = nullfn_t, typename MutateType = nullfn_t>
+class property :
+  public property_base<Type, AccessType, MutateType>
+{
+  private:
+    using base_type = property_base<Type,AccessType,MutateType>;
 
   public:
-    template <typename Accessor, typename Mutator>
-    property (Type init, Accessor accessor, Mutator mutator) :
-      property_ {std::forward<Type> (init)},
-      accessor_ {std::forward<Accessor> (accessor)},
-      mutator_ {std::forward<Mutator> (mutator)} {}
+    using base_type::property_base;
 
   public:
     template <typename ...Args>
     auto get (Args ...args)
     {
-      return accessor_ (property_, std::forward<Args>(args)...);
+      return base_type::accessor_ (base_type::property_, std::forward<Args>(args)...);
     }
 
     template <typename ...Args>
     auto set (Args ...args)
     {
-      return mutator_ (property_, std::forward<Args>(args)...);
+      return base_type::mutator_ (base_type::property_, std::forward<Args>(args)...);
     }
-
-  private:
-    Type property_;
-
-  private:
-    access_method accessor_;
-    mutate_method mutator_;
 };
 
 int main()
 {
-  int init = 4;
-  property<int&, int(void), void(int)> p
+  property<int, int(void), void(int)> p
   {
-    init,
+    10,
     [](int &value) { return value + 1; },
     [](int &value, int v) { value = v / 2; }
   };
 
-  p.set(8);
+  cout << p.get() << endl;
+  p.set(5);
   cout << p.get() << endl;
 
   return 0;
