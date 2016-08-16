4ab789c0f72f1504b595dc36ef5f5b7e58df38a2
Ryan McDougall <sempuki1@gmail.com>
2016-08-16 13:56:17 -0700

    add array-absurdity

--- exercises/codeeval/moderate/array-absurdity.cpp
+++ exercises/codeeval/moderate/array-absurdity.cpp
@@ -4,11 +4,31 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 void process(std::string &&line) {
     std::stringstream stream{std::move(line)};
+    std::string buffer;
+    std::vector<int> array;
 
-    // process here
+    std::getline(stream, buffer, ';');
+    array.reserve(std::stoi(buffer));
+    while (std::getline(stream, buffer, ',')) {
+        array.push_back(std::stoi(buffer));
+    }
+
+    bool found = false;
+    size_t index = 0;
+    while (index < array.size()) {
+        while (index != array[index]) {
+            if (array[index] == array[array[index]]) {
+                std::cout << array[index] << std::endl;
+                return;
+            }
+            std::swap(array[index], array[array[index]]);
+        }
+        index++;
+    }
 }
 
 int main(int argc, char **argv) {
