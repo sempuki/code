e6b06932977c6e0dda722b17784e032d7149d2a8
Ryan McDougall <sempuki1@gmail.com>
2022-02-05 19:58:54 -0500

    update error

--- error/main.cpp
+++ error/main.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 
-#include "hazard.hpp"
+#include "error.hpp"
 
 ctl::posix_domain posix;
 ctl::win32_domain win32;
 
-ctl::hazard foo() {
-    return posix.raise(ctl::posix_condition::PERM,  //
-                       std::source_location::current(),
-                       "send help!");
+ctl::error foo() {
+  return posix.raise(ctl::posix_condition::PERM,  //
+                     std::source_location::current(),
+                     "send help!");
 }
 
-ctl::hazard bar() {
-    return win32.raise(ctl::win32_condition::ACCESS_DENIED,  //
-                       std::source_location::current(),
-                       "another one bits the dust.");
+ctl::error bar() {
+  return win32.raise(ctl::win32_condition::ACCESS_DENIED,  //
+                     std::source_location::current(),
+                     "another one bites the dust.");
 }
 
-void print(ctl::hazard h) {
-    std::cerr << "raise hazard " << h.message() << " at " << h.location().file_name() << ":"
-              << h.location().line() << "\n";
+void print(ctl::error e) {
+  std::cerr << "raise error " << e.message() << " at " << e.location().file_name() << ":"
+            << e.location().line() << "\n";
 }
 
 int main() {
-    auto no_permission0 = posix.expect(ctl::posix_condition::PERM);
-    auto no_permission1 = win32.expect(ctl::win32_condition::ACCESS_DENIED);
+  auto no_permission0 = posix.expect(ctl::posix_condition::PERM);
+  auto no_permission1 = win32.expect(ctl::win32_condition::ACCESS_DENIED);
 
-    auto hazard0 = foo();
-    if (hazard0 == no_permission0) {
-        print(hazard0);
-    }
+  auto error0 = foo();
+  if (error0 == no_permission0) {
+    print(error0);
+  }
 
-    auto hazard1 = bar();
-    if (hazard1 == no_permission1) {
-        print(hazard1);
-    }
+  auto error1 = bar();
+  if (error1 == no_permission1) {
+    print(error1);
+  }
 
-    return 0;
+  return 0;
 }
