0f61436cccff096334df81da7d43091fc1947fa1
Ryan McDougall <sempuki1@gmail.com>
2022-09-01 20:13:13 -0700

    update exercises

--- exercises/simple_lru_cache.cpp
+++ exercises/simple_lru_cache.cpp
@@ -3,79 +3,107 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <iostream>
 #include <list>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 template <typename Key, typename Value, size_t Capacity>
-class FlatLruCache {
+class FlatLruCache final {
  public:
   Value& operator[](const Key& key) {
-    size_t index = 0;
-    const auto iter = std::find(keys_.begin(), keys_.end(), key);
-    if ((iter == keys_.end()) || (index = std::distance(keys_.begin(), iter), times_[index] == 0)) {
-      index = std::distance(times_.begin(), std::min_element(times_.begin(), times_.end()));
+    auto [index, found] = try_find(key);
+    if (!found) {
+      index =
+        std::distance(use_count_.begin(), std::min_element(use_count_.begin(), use_count_.end()));
+      use_count_[index] = 0;
       keys_[index] = key;
     }
-    times_[index]++;
+    use_count_[index]++;
     return values_[index];
   }
 
   bool has(const Key& key) {
-    const auto iter = std::find(keys_.begin(), keys_.end(), key);
-    return iter != keys_.end() && times_[std::distance(keys_.begin(), iter)] != 0;
+    const auto [_, found] = try_find(key);
+    return found;
   }
 
   bool invalidate(const Key& key) {
+    const auto [index, found] = try_find(key);
+    if (found) {
+      use_count_[index] = 0;
+      values_[index] = {};
+    }
+    return found;
+  }
+
+ private:
+  std::pair<size_t, bool> try_find(const Key& key) {
     const auto iter = std::find(keys_.begin(), keys_.end(), key);
     if (iter != keys_.end()) {
-      size_t index = std::distance(keys_.begin(), iter);
-      times_[index] = 0;
-      values_[index] = {};
+      const auto index = std::distance(keys_.begin(), iter);
+      if (use_count_[index] != 0u) {
+        return {index, true};
+      }
+    }
+    return {0u, false};
+  }
+
+  std::array<Key, Capacity> keys_;
+  std::array<Value, Capacity> values_;
+  std::array<uint64_t, Capacity>
+    use_count_{};  // Zero is a sentinel value that means `unused` or `invalid`.
+};
+
+template <typename Key, typename Value, size_t Capacity>
+class HashLruCache final {
+ public:
+  HashLruCache() {
+    for (size_t i = 0u; i < Capacity; ++i) {
+      used_order_.push_back(&entries_[i]);
+      entries_[i].used = std::prev(used_order_.end());
+    }
+  }
+
+  HashLruCache(const HashLruCache&) = delete;
+  HashLruCache& operator=(const HashLruCache&) = delete;
+
+  Value& operator[](const Key& key) {
+    const auto iter = keys_.find(key);
+    const auto used = (iter != keys_.end())
+                        ? iter->second->used
+                        : (keys_[key] = used_order_.back(), std::prev(used_order_.end()));
+
+    used_order_.splice(used_order_.begin(), used_order_, used);
+    return (*used)->value;
+  }
+
+  bool has(const Key& key) { return keys_.find(key) != keys_.end(); }
+
+  bool invalidate(const Key& key) {
+    const auto iter = keys_.find(key);
+    if (iter != keys_.end()) {
+      iter->second->value = {};
+      keys_.erase(iter);
       return true;
     }
     return false;
   }
 
  private:
-  std::array<uint64_t, Capacity> times_{};
-  std::array<Key, Capacity> keys_{};
-  std::array<Value, Capacity> values_{};
-};
+  struct Entry {
+    Value value;
+    typename std::list<Entry*>::iterator used;
+  };
 
-template <typename Key, typename Value, size_t Capacity>
-class OrderedLruCache {
- public:
-  Value& operator[](const Key& key) {
-    const auto iter = keys_.find(key);
-    if (iter != keys_.end()) {
-      values_.splice(values_.begin(), values_, iter);
-      return *iter;
-    } else {
-      if (values_.size() == Capacity) {
-        keys_.erase();
-      }
-      values_.emplace_front();
-      keys_[key] = values_.begin();
-      return values_.front();
-    }
-  }
-
-  bool has(const Key& key) {
-    return keys_.find(key) != keys_.end();  //
-  }
-
-  bool invalidate(const Key& key) {
-    //
-  }
-
- private:
-  std::unordered_map<Key, typename std::list<Value>::iterator> keys_;
-  std::list<Value> values_;
+  std::array<Entry, Capacity> entries_;
+  std::unordered_map<Key, Entry*> keys_;
+  std::list<Entry*> used_order_;
 };
 
 int main() {
-  using T = FlatLruCache<std::string, int, 3>;
+  using T = HashLruCache<std::string, int, 3>;
   {
     T cache;
     {  // It should have a key for value.
