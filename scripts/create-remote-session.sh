65d4658cf60da84b77f35bf7245997f37fb8c9e4
Ryan McDougall <mcdougall.ryan@gmail.com>
2024-11-14 12:22:01 -0800

    Update create-remote-session.sh

--- scripts/create-remote-session.sh
+++ scripts/create-remote-session.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh $@ -t tmux new 'nvim -u ~/.vimrc --cmd ":cd ~/Work/av/ | :set nonumber | :exe \"term\" | :vsplit | :exe \"term\" | :split | :exe \"term\" | :tabnew | :set number"'
+ssh $@ -t tmux new 'nvim -u ~/.vimrc --cmd ":cd ~/Repos | :set nonumber | :set shell=/bin/bash | :exe \"term\" | :vsplit | :exe \"term\" | :split | :exe \"term\" | :tabnew | :set number"'
