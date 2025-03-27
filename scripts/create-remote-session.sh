44c0cd314c61d37c617ba08c9b5ea47d0f7d3543
Ryan McDougall <rymcdougall@nvidia.com>
2025-03-27 16:41:22 -0700

    update scripts

--- scripts/create-remote-session.sh
+++ scripts/create-remote-session.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 REMOTE_CWD=${1}
 shift # Pop off first argument.
-ssh ${@} -t tmux new 'nvim -u ~/.vimrc --cmd ":cd '${REMOTE_CWD}' | :set nonumber | :set shell=/bin/bash | :exe \"term\" | :vsplit | :exe \"term\" | :split | :exe \"term\" | :tabnew | :set number"'
+ssh ${@} -t tmux new -s $(basename ${REMOTE_CWD}) \
+  'nvim -u ~/.vimrc --cmd ":cd '${REMOTE_CWD}' | :set nonumber | :set shell=/bin/bash | :exe \"term\" | :vsplit | :exe \"term\" | :split | :exe \"term\" | :tabnew | :set number"'
